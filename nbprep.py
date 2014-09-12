645f7c500fc95b5a54f21900f9233f9f087b4698
Ryan Nelson <rnelsonchem@gmail.com>
2014-09-11 23:49:41 -0400

    Fixed custom.css display
    I needed to fix my styling function so that it would properly display
    the css I needed.

--- nbprep.py
+++ nbprep.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
-from IPython.core.display import HTML
+from IPython.core.display import display, HTML
 
 def style(dpi=120):
     plt.rc('savefig', dpi=dpi)
     css_file = './static/custom.css'
-    HTML(open(css_file, "r").read())
+    css = HTML(open(css_file, "r").read())
+    display( css )
     
 
