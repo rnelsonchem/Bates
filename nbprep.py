418310fd9dffb840ff5f06f131f20a48829c46e1
Ryan Nelson <rnelsonchem@gmail.com>
2014-09-14 19:16:22 -0400

    Finished marking up the plot/fit notebook
    Finally got explanatory text in for the entire first notebook. I made a
    change to custom.css to render in-line code a slightly different color.
    I also dumped all of the output before saving so that it wouldn't be
    uploaded with the notebook.
    
    I was going to add automatic numbering to the MathJax equations, but I
    decided against it. Left the commented code in nbprep.py in case I
    change my mind later.

--- nbprep.py
+++ nbprep.py
@@ -1,13 +1,22 @@
 import os
 
 import matplotlib.pyplot as plt
-from IPython.core.display import display, HTML
+from IPython.core.display import display, HTML, Javascript
 
 def style(dpi=120):
     plt.rc('savefig', dpi=dpi)
+
     css_file = './static/custom.css'
     css = HTML(open(css_file, "r").read())
     display( css )
+# Uncomment this stuff for equation numbering
+#    mathjax = \
+#'''
+#MathJax.Hub.Config({
+#  TeX: { equationNumbers: { autoNumber: "AMS" } }
+#});
+#'''
+#    display( Javascript(mathjax))
     
 def dump_sync(folder='.'):
     files = os.listdir(folder)
